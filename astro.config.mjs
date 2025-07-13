06eb885c5113dc20fe1e279fa2de59def5ef1f92
hinkoko <piotr62@live.fr>
2025-07-13 20:27:47 +0200

    power to the top

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site : 'https://cee-sud-ouest.fr',
+  integrations : [sitemap()],
 });
